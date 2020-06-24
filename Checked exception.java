00df9969da820d9ce30f482de55c3877a043447a
mahiambekar <66378229+mahiambekar@users.noreply.github.com>
2020-06-24 17:56:26 +0530

    Add files via upload

---
+++ Checked exception.java
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+import java.io.FileInputStream;
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+		FileInputStream fin;
+		try{
+		    fin=new FileInputStream("c:\\listfiles\\test.txt");
+		}
+		catch(Exception e) {
+	  }
+	}
+}
