cae68e6e49eb6720150e5f556d158c006b1a9dd5
mahiambekar <66378229+mahiambekar@users.noreply.github.com>
2020-06-24 17:25:50 +0530

    Add files via upload

---
+++ Exception handling throw block.java
@@ -0,0 +1,31 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner ob=new Scanner(System.in);
+		int a,b,c;
+		System.out.println("Enter two numbers");
+		a=ob.nextInt();
+		b=ob.nextInt();
+		try{
+		    if(b==0){
+		        ArithmeticException ex=new ArithmeticException("Divide by zero");
+		        throw ex;
+		   
+		    }
+		    c=a/b;
+		    System.out.println(c);
+		}
+		catch(ArithmeticException e){
+		    System.out.println(e);
+		}
+		
+	}
+}
