460b3110237eabf42602161d688d9a103675d921
mahiambekar <66378229+mahiambekar@users.noreply.github.com>
2020-06-24 16:54:59 +0530

    Add files via upload

---
+++ Exception handling 2.java
@@ -0,0 +1,31 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+    import java.util.Scanner;
+
+
+    public static void main(String[] args) {
+        Scanner ob = new Scanner(System.in);
+        int a, b, c;
+        System.out.println("Enter 2 numbers");
+        a = ob.nextInt();
+        b = ob.nextInt();
+        try {
+            c = a / b;
+            System.out.println(c);
+
+        } catch (ArithmeticException e) {
+            System.out.println("Cannot divide by 0");
+        }
+        System.out.println("Hello");
+    }
+
+}
+
